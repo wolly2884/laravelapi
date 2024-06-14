bb5d17aaf3c57e1e8a254c1868ba9289c00cd4b6
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-06-14 03:08:36 +0000

    ok

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '43a12b9bb0d711b28a7772fd65a1a94c709e73e9',
+        'reference' => 'e4f5f0286ae12bd84aaf98fca63821392efb15cc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '43a12b9bb0d711b28a7772fd65a1a94c709e73e9',
+            'reference' => 'e4f5f0286ae12bd84aaf98fca63821392efb15cc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -748,8 +748,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
